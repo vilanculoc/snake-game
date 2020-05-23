5f0451b70ad698c1398b1cf01bbf8296d62f53da
celsa Vilanculo <vilanculo.celsa@gmail.com>
2020-05-24 00:06:09 +0200

    Fiz com que a cobrinha reapareca quando terminar o plano

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function criarCobrinha (){
 }
 
 
-
+// definir as accoes realizadas pelas teclas de direccao do key board
 document.addEventListener('keydown',update);
 
 function update(event){
@@ -47,6 +47,10 @@ function setSpeed(){
 */
 
 function iniciarJogo(){
+	if(snake[0].x > 15*box && direction =="right") snake[0].x = 0;
+	if(snake[0].x < 0 && direction =="left") snake[0].x =16*box;
+	if(snake[0].y > 15*box && direction =="down") snake[0].y =0;
+	if(snake[0].x < 0 && direction =="up") snake[0].y = 16 * box;
 	
 	criarBG();
 	criarCobrinha();	
